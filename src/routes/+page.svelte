6f8145d397c8d799578ee65490f93899510652a5
DUCHIER Denys <p11059@info03-01.iut45.univ-orleans.fr>
2022-12-16 16:25:14 +0100

    pouchdb et grille

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TopAppBar, {
+      Row,
+      Section,
+      Title,
+      AutoAdjust,
+    } from '@smui/top-app-bar';
+    import IconButton from '@smui/icon-button';
+    let topAppBar;
+    import TinyPouchDB from '../TinyPouchDB.svelte';
+    let books=[];
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
+</script>
+<TopAppBar bind:this={topAppBar} variant="fixed">
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Fixed</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+<TinyPouchDB bind:documents={books} initsrc="/books.json" collection="books"></TinyPouchDB>
+<LayoutGrid>
+    {#each books as book}
+      <Cell span={1}>
+        <div class="demo-cell"><img src="data:{book.img.content_type};base64,{book.img.data}"
+            alt="{book.title}"></div>
+      </Cell>
+    {/each}
+  </LayoutGrid>
+  <style>
+    .demo-cell {
+      /*height: 60px;*/
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: var(--mdc-theme-secondary, #333);
+      color: var(--mdc-theme-on-secondary, #fff);
+    }
+  </style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	resolve: {
+		alias: {
+			events: 'rollup-plugin-node-polyfills/polyfills/events'
+		}
+	},
+	plugins: [
+		sveltekit(),
+		rollupNodePolyFill()
+	]
 };
 
 export default config;
